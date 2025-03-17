b8f005419b3c97531a56396853b6533638a97205
1DelFin1 <defario777@mail.ru>
2025-03-17 22:36:41 +0500

    change init models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,15 @@
 __all__ = (
     "UserModel",
     "TestModel",
+    "QuestionModel",
+    "OptionModel",
+    "ResultModel",
 )
 
 from src.models.users import UserModel
-from src.models.tests import TestModel
+from src.models.tests import (
+    TestModel,
+    QuestionModel,
+    OptionModel,
+    ResultModel,
+)
