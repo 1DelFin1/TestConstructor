56ba01f4e789a6e70b635e4285953d7f317dff5c
1DelFin1 <defario777@mail.ru>
2025-03-20 09:51:06 +0500

    add models to init

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,15 +1,21 @@
 __all__ = (
     "UserModel",
+    "TestedUserModel",
     "TestModel",
     "QuestionModel",
     "OptionModel",
     "ResultModel",
+    "TestedUserModel",
 )
 
-from src.models.users import UserModel
+from src.models.users import (
+    UserModel,
+    TestedUserModel,
+)
 from src.models.tests import (
     TestModel,
     QuestionModel,
     OptionModel,
     ResultModel,
+    TestedUserModel,
 )
